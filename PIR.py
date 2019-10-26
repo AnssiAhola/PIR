f8c96377e502b15a5ec04eb5485e3559ef6faf46
AnssiAhola <anssia.ahola@gmail.com>
2019-10-26 23:55:51 +0300

    more refactoring, implemented --rotate

--- PIR.py
+++ PIR.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-import os
-import re
-import sys
-import math
-import time
-import argparse
-import concurrent.futures
+from os import path, listdir, mkdir
+from re import match
+from sys import argv
+from math import floor
+from time import perf_counter
+from argparse import ArgumentParser, ArgumentTypeError
+from concurrent.futures import ThreadPoolExecutor
 from io import BytesIO
 from progress.bar import Bar
 from PIL import Image, ImageOps, ExifTags, ImageFile
@@ -13,28 +13,26 @@ from PIL import Image, ImageOps, ExifTags, ImageFile
 # Fixes "IOError: broken data stream when reading image file" when loading files concurrently
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-valid_ext = ['.jpg', '.jpeg', '.png']
+supported = ['.jpg', '.jpeg', '.png']
+
 
 # Check if input folder/file exists
-
-
 def exists(s):
-    if not os.path.exists(s):
-        raise argparse.ArgumentTypeError('path does not exist')
+    if not path.exists(s):
+        raise ArgumentTypeError('path does not exist')
     else:
         return s
 
+
 # Validate resolution (WidthxHeight OR Width)
-
-
 def resolution(res):
-    if re.match(r'^[0-9]+x*[0-9]+$', res):
+    if match(r'^[0-9]+x*[0-9]+$', res):
         return res
     else:
-        raise argparse.ArgumentTypeError('&d' % res)
+        raise ArgumentTypeError('&d' % res)
 
 
-parser = argparse.ArgumentParser(
+parser = ArgumentParser(
     description='Example. PIR.py -i ./inputfolder -o ./outputfolder 2560x1440 1920 100x100 --resize')
 parser.add_argument(
     '-i', '--input', type=exists, help='Input file or folder', required=True)
@@ -50,18 +48,18 @@ parser.add_argument(
     '--verbose', help='Enable detailed logging', action='store_true')
 parser.add_argument(
     '--organize',
-    help='Organize output resolutions in their own folders, disables the resolution suffix',
+    help='Organize output resolutions in their own folders, disables the resolution suffix on files',
     action='store_true')
 
 # Image processing arguments
+group = parser.add_mutually_exclusive_group(required=True)
 parser.add_argument(
     '--rotate', help='Rotate image(s) if neccesary', action='store_true')
-group = parser.add_mutually_exclusive_group(required=True)
 group.add_argument(
     '--resize', help='resize images, maintains aspect ratio', action='store_true')
 group.add_argument(
     '--crop',
-    help='crop images, centered, NOTE: Skips images with width OR height greater or equal to target',
+    help='crop images, centered, NOTE: Skips images with width OR height smaller or equal to target',
     action='store_true')
 
 args = parser.parse_args()
@@ -70,44 +68,49 @@ args = parser.parse_args()
 def main(argv):
     skipped = 0
     try:
-        input_dir = os.path.realpath(args.input)
-        output_dir = os.path.realpath(args.output)
+        input_dir = path.realpath(args.input)
+        output_dir = path.realpath(args.output)
         resolutions = args.resolution
         for i, res in enumerate(resolutions):
             resolutions[i] = tuple(map(lambda r: int(r), res.split('x')))
             if len(resolutions[i]) is 1:
                 resolutions[i] = (resolutions[i][0], resolutions[i][0])
 
-        if any(ext in input_dir for ext in valid_ext):
+        # Filter out unsupported files
+        if any(ext in input_dir for ext in supported):
             files = [input_dir.split('\\')[-1]]
-            input_dir = os.path.dirname(os.path.realpath(args.input))
+            input_dir = path.dirname(path.realpath(args.input))
+        elif path.isdir(input_dir):
+            files = [file for file in listdir(input_dir) if any(
+                ext in file for ext in supported)]
         else:
-            files = [file for file in os.listdir(input_dir)]
+            raise OSError(0, 'Input filetype is not supported')
 
         if args.verbose:
             print('Input:               ', input_dir)
             print('Output:              ', output_dir)
             print('Rotate:              ', args.rotate)
             print('Resolution(s):       ', *resolutions, '\n')
-            print(*files, sep='\n')
+            print('File(s):\n', *files, sep='\n ')
+            print()
             if not args.yes:
-                input('\nPress anything to continue, Ctrl+C to cancel...')
+                input('Press any key to continue, Ctrl+C to cancel...')
 
-        start = time.perf_counter()
+        start = perf_counter()
 
         # Process imagefiles
         action = 'Resizing' if args.resize else 'Cropping'
         for res in resolutions:
             with Bar('%s (%dx%d)' % (action, *res), max=len(files)) as bar:
                 if args.organize:
-                    save_to = os.path.join(output_dir, '%sx%s' % res)
+                    output_dir = path.join(output_dir, '%sx%s' % res)
                 else:
-                    save_to = output_dir
-                if not os.path.exists(save_to):
-                    os.mkdir(save_to)
-                with concurrent.futures.ThreadPoolExecutor(max_workers=6) as executor:
+                    output_dir = output_dir
+                if not path.exists(output_dir):
+                    mkdir(output_dir)
+                with ThreadPoolExecutor(max_workers=6) as executor:
                     def _helper(file):
-                        return process_img(file, input_dir, save_to, res)
+                        return process_img(file, input_dir, output_dir, res)
                     for file, success in zip(files, executor.map(_helper, files)):
                         # Count how many images skipped
                         if not success:
@@ -116,32 +119,39 @@ def main(argv):
         if skipped > 0:
             print(f'\nSkipped {skipped} file(s)')
 
-        end = time.perf_counter()
+        end = perf_counter()
 
         print(f'\nFinished in {(end-start):.2f} seconds')
 
     except KeyboardInterrupt:
         print('\nProcess cancelled\n')
+    except OSError as err:
+        print(err)
 
 
-def process_img(file, input_dir, save_to, res, quality=95):
+def process_img(file, input_dir, output_dir, res, quality=95):
     try:
-        image = Image.open(os.path.join(input_dir, file))
+        image = Image.open(path.join(input_dir, file))
         target_W, target_h = res
         width, height = image.size
-        name, ext = os.path.splitext(file)
+        name, ext = path.splitext(file)
 
-        # Skip images with resolution greater or equal to target resolution
+        # When cropping skip images with resolution smaller or equal to target resolution
         if args.crop and (target_W >= width or target_h >= height):
             return False
 
-        image = resize(image, res) if args.resize else crop(image, res)
+        if args.resize:
+            image = resize(image, res)
+        elif args.crop:
+            image = ImageOps.fit(image, res, Image.ANTIALIAS, 0, (0.5, 0.5))
+        if args.rotate:
+            image = rotate(image)
 
         # Add file suffix (resolution)
         if not args.organize:
-            file = name + (f' - {target_W}x{target_h}') + ext
+            file = name + (' - %dx%d' % image.size) + ext
 
-        image.save(os.path.join(save_to, file), image.format, quality=quality)
+        image.save(path.join(output_dir, file), image.format, quality=quality)
         return True     # Success
 
     except IOError as err:
@@ -153,19 +163,27 @@ def resize(image, res):
     max_width, max_height = res
     width, height = image.size
     resize_ratio = min(max_width/width, max_height/height)
-    new_size = (math.floor(resize_ratio * width),
-                math.floor(resize_ratio * height))
+    new_size = (floor(resize_ratio * width), floor(resize_ratio * height))
     return image.resize(new_size, Image.ANTIALIAS)
 
 
-def crop(image, res):
-    return ImageOps.fit(image, res, Image.ANTIALIAS, 0, (0.5, 0.5))
-
-
 def rotate(image):
+    try:
+        # https://stackoverflow.com/a/6218425
+        # key = orientation value from images exif tags
+        # value = degrees to to rotate the image for correct orientation
+        degrees = {3: 180, 6: 270, 8: 90}
+        # https://github.com/python-pillow/Pillow/blob/master/src/PIL/ExifTags.py
+        # Constant for exif tag for Orientation
+        ORIENTATION = 0x0112
 
-    return image
+        exif_data = dict(image._getexif().items())
+        deg_to_rotate = degrees[exif_data[ORIENTATION]]
+
+        image = image.rotate(deg_to_rotate, expand=True)
+    finally:
+        return image
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main(argv[1:])
